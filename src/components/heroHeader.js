47fe695fef213a5c62c7cd697eced20881620b12
Gerrishon Sirere <s.gerrishon@gmail.com>
2022-08-03 16:13:27 +0300

    Update heroHeader.js

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -22,6 +22,7 @@ export default () => (
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+        <Link to='/team' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
   />
